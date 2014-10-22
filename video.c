d16c02ab9b0e8c36a94c03ebb08ab4787d967879
alexandremumble <alexandremumble@gmail.com>
2014-10-22 16:23:33 +0200

    correction video.c (ajouts des dépendances + divers fixes)

--- video.c
+++ video.c
@@ -1,5 +1,10 @@
 #include "video.h"
 #include <pthread.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 //adresses pour la com vidéo
 struct sockaddr_in addr_drone, addr_client;
@@ -11,7 +16,7 @@ fd_set vid_fd_set;
 struct timeval video_timeout = {4, 0};
 static volatile int stopped = 1;
 
-char[TCP_VIDEO_BUF_SIZE] tcp_buf;
+char tcp_buf[TCP_VIDEO_BUF_SIZE];
 
 //cette fonction est un thread, ne pas l'appeler !
 void* video_routine(void* args) {
@@ -24,10 +29,10 @@ void* video_routine(void* args) {
 		}
 		else if(FD_ISSET(sock_video, &vid_fd_set)) {
 			//recevoir le paquet du drone
-			pack_size = recv(sock_video, tcp_buf, TCP_BUF_SIZE, 0);
+			pack_size = recv(sock_video, tcp_buf, TCP_VIDEO_BUF_SIZE, 0);
 			if (pack_size < 0)
 				perror("Erreur recv()");
-			printf("Reçu %d octets de vidéo.\n");
+			printf("Reçu %d octets de vidéo.\n", pack_size);
 		}
 		else {
 			printf("Timeout : aucune donnée vidéo reçue.\n");
@@ -36,8 +41,8 @@ void* video_routine(void* args) {
 	}
 	pthread_exit(NULL);
 }
-		
-		
+
+
 
 int jakopter_init_video(lua_State* L) {
 /*
@@ -50,23 +55,23 @@ int jakopter_init_video(lua_State* L) {
 	addr_client.sin_family      = AF_INET;
 	addr_client.sin_addr.s_addr = htonl(INADDR_ANY);
 	addr_client.sin_port        = htons(PORT_VIDEO);
-	
+
 	sock_video = socket(AF_INET, SOCK_DGRAM, 0);
 	if(sock_video < 0) {
 		fprintf(stderr, "Erreur, impossible d'établir le socket\n");
 		lua_pushnumber(L, -1);
 		return 1;
 	}
-	
+
 	//bind du socket client pour le forcer sur le port choisi
-	if(connect(sock_video, (struct sockaddr*)&addr_drone, sizeof(addr_drone)) == SOCKET_ERROR) {
+	if(connect(sock_video, (struct sockaddr*)&addr_drone, sizeof(addr_drone)) < 0) {
 		perror("Erreur vidéo connect()");
 		lua_pushnumber(L, -1);
 		return 1;
 	}
 	//ajouter le socket au set pour select
 	FD_SET(sock_video, &vid_fd_set);
-	
+
 	stopped = 0;
 	//démarrer la réception des packets vidéo
 	if(pthread_create(&video_thread, NULL, video_routine, NULL) < 0) {
@@ -75,7 +80,7 @@ int jakopter_init_video(lua_State* L) {
 		lua_pushnumber(L, -1);
 		return 1;
 	}
-	
+
 	lua_pushnumber(L, 0);
 	return 1;
 }
@@ -85,4 +90,3 @@ int jakopter_stop_video(lua_State* L) {
 	close(sock_video);
 	lua_pushnumber(L, pthread_join(video_thread, NULL));
 }
-
