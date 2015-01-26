90a75dcd957e4c893f38e2e3dc198522e48fb805
hecfac <hector.labanca@etudiant.univ-rennes1.fr>
2015-01-26 22:48:19 +0100

    Add preliminary lua bindings for com channel.

--- lua_bindings.c
+++ lua_bindings.c
@@ -1,8 +1,20 @@
 #include "drone.h"
 #include "navdata.h"
 #include "video.h"
+#include "com_channel.h"
 #include "lauxlib.h"
 #include "lua.h"
+
+/*
+* Lua helper functions
+*/
+//check whether the first argument is a com channel.
+jakopter_com_channel_t** check_com_channel(lua_State* L)
+{
+	void* arg = luaL_checkudata(L, 1, "jakopter.com_channel");
+	luaL_argcheck(L, arg != NULL, 1, "com_channel expexted");
+	return (jakopter_com_channel_t**)arg;
+}
 
 /*
 Lua bindings to user-exposed functions
@@ -85,6 +97,28 @@ int jakopter_stay_lua(lua_State* L){
 	lua_pushnumber(L, jakopter_stay());
 	return 1;
 }
+int jakopter_init_com_channel_lua(lua_State* L){
+	lua_Integer s = luaL_checkinteger(L, 1);
+	luaL_argcheck(L, s > 0, 1, "Channel size must be > 0");
+	//store the cc pointer as user data, so that we can assign our custom metatable to it.
+	jakopter_com_channel_t** cc = lua_newuserdata(L, sizeof(jakopter_com_channel_t*));
+	luaL_getmetatable(L, "jakopter.com_channel");
+	lua_setmetatable(L, -2);
+	//actually create the channel. Raise an error if it fails.
+	*cc = jakopter_init_com_channel((size_t)s);
+	if(*cc == NULL)
+		return luaL_error(L, "Failed to create a com_channel of size %d", s);
+	
+	return 1;	
+}
+int jakopter_destroy_com_channel_lua(lua_State* L){
+	jakopter_com_channel_t** cc = check_com_channel(L);
+	jakopter_destroy_com_channel(cc);
+	if(*cc != NULL)
+		return luaL_error(L, "Failed to destroy the com_channel");
+		
+	return 0;
+}
 
 
 //enregistrer les fonctions pour lua
@@ -107,10 +141,15 @@ static const luaL_Reg jakopterlib[] = {
 	{"ftrim", jakopter_ftrim_lua},
 	{"calib", jakopter_calib_lua},
 	{"stay", jakopter_stay_lua},
+	{"create_cc", jakopter_init_com_channel_lua},
+	{"destroy_cc", jakopter_destroy_com_channel_lua},
 	{NULL, NULL}
 };
 
 int luaopen_drone(lua_State* L) {
+	//the metatable is used for type-checking our custom structs in lua.
+	//here, define a table for com channels pointers.
+	luaL_newmetatable(L, "jakopter.com_channel");
 	//lua 5.1 et 5.2 incompatibles...
 #if LUA_VERSION_NUM <= 501
 	luaL_register(L, "jakopter", jakopterlib);
