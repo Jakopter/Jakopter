093eff630cf8b1c3df1f8a9f8dcccac903f8b639
hecfac <hector.labanca@etudiant.univ-rennes1.fr>
2015-04-01 15:52:52 +0200

    keyb_control changes, non-blocking input tentative, untested

--- src/cmd.c
+++ src/cmd.c
@@ -1,34 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
+#include <sys/poll.h>
 
 #define CMDFILENAME "/tmp/jakopter_cmd.txt"
+#define INPUT_INTERVAL (1/30)*1000
 
 int main(){
-	char c;
-	int i = 100;
+
+	struct pollfd keyb_poll;
+	//by default, 'a' = stay/don't move
+	char c = 'a';
 	FILE *cmd;
 	initscr();
 
-	while (1) {
-		c = getch();
-
-		if (c == 's') {
-			endwin();
-			return 0;
+	//initialize the poll structure so that it checks keyboard input
+	keyb_poll.fd = stdin;
+	keyb_poll.events = POLLIN;
+	
+	while (c != 's') {
+		
+		//put the input back to "neutral" (no movement)
+		c = 'a';
+		
+		//check keyboard input
+		if (poll(&keyb_poll, 1, INPUT_INTERVAL) > 0
+			&& keyb_poll.revents & POLLIN) {
+			c = getch();
 		}
-
+		
+		//print the new value to the input file
 		cmd = fopen(CMDFILENAME, "w");
 		fprintf(cmd, "%c\n", (int) c);
 		fclose(cmd);
 	}
 
-	i--;
 
-	if (i <0) {
-		endwin();
-		return 0;
-	}
-
-	return 0;
 }
